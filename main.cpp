a422c130cde34c45ae9c9e38a4b9749461c269fa
rita <rita@gmail.com>
2015-12-03 15:59:33 +0300

    Добавили ввод целых чисел

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include "sdt.h"
+#include <iostream>
+
+using namespace std;
 
 void main() {
-	
+	int x, y;
+
+	cout << "please input x" << endl;
+	cin >> x;
+	cout << "please input y" << endl;
+	cin >> y;
+
+	cout << "x: " << x << "y: " << y;		
 }
